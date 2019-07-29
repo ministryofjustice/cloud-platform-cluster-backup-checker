ac247e703de8026ee5d80fec8f7afdb2359e933f
Poornima Krishnasamy <poornima.krishnasamy@digital.justice.gov.uk>
2019-07-29 12:10:56 +0100

    Updated Readme for more instructions and comments

--- bin/ruby-list-snapshots.rb
+++ bin/ruby-list-snapshots.rb
@@ -9,6 +9,11 @@ require "slack-notifier"
 
 DAY_IN_SECONDS = 86400
 
+#####################################################
+# get_snapshots() uses AWS API describe_snapshots to get 
+# the list of snapshots with the filters given in 'opts'. 
+# 'result' is looped until the next_token string is null. 
+######################################################  
 def get_snapshots(client, opts)
   snapshots = []
   next_token = "dummy"
@@ -25,7 +30,12 @@ def get_snapshots(client, opts)
 end
 
 
-
+#####################################################
+# send_message() calls the slack-notifier wrapper 
+# https://github.com/stevenosloan/slack-notifier to send 
+# notifications to respective slack channel for 
+# the given title and text
+###################################################### 
 def send_message(title, text)
   notifier = Slack::Notifier.new ENV.fetch('SLACK_WEBHOOK') do
       defaults channel: "#lower-priority-alarms",
@@ -57,17 +67,17 @@ end
 
 
 #####################################################
+# This script uses IAM role with permissions and uses 
+# AssumeRole to create credentials dynamically in 'role_credentials'
+######################################################
 
 role_arn = "arn:aws:iam::"+ENV.fetch('ACCOUNT_ID')+":role/"+ENV.fetch('ROLE_NAME')       
 role_session_name = "cluster_backup_checker_session"
-puts role_arn
 
 role_credentials = Aws::AssumeRoleCredentials.new(
   role_arn: role_arn,
   role_session_name: role_session_name
 )
-
-puts role_credentials
 
 client = Aws::EC2::Client.new(
   region: ENV.fetch('AWS_REGION'),
@@ -81,14 +91,18 @@ opts = {
   ]
 }
 
+# call get_snapshots method
 snapshots = get_snapshots(client, opts)
 
+# sort the snapshots returned by 'start_time' and 
+# fetch the most_recent snapshot
 most_recent = snapshots.sort_by { |snapshot| snapshot.start_time }.last
 
 age = Time.now.to_i - most_recent.start_time.to_i
 
 days_old = age / DAY_IN_SECONDS
 
+#compare the time to check if the snapshot is one day old
 current_time = Time.now
 if days_old > 1
   send_message("Cluster backup failure", "Cluster snapshots backups not recent")
